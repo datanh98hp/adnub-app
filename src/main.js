c5201ea5f89255ee69ec040a26fcf86f8bf02ecb
datanh98hp <dat198hp@gmail.com>
2023-06-13 00:36:10 +0700

    first commit

---
+++ src/main.js
@@ -0,0 +1,20 @@
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+
+import App from './App.vue'
+import router from './router'
+// import { BootstrapVue, BootstrapVueIcons } from 'bootstrap-vue'
+
+// import 'bootstrap/dist/css/bootstrap.css'
+// import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+const app = createApp(App)
+
+// app.use(BootstrapVue)
+// app.use(BootstrapVueIcons)
+app.use(createPinia())
+app.use(router)
+
+app.mount('#app')
---
+++ src/pages/ManagerWallpapers.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        <h2>ấijd</h2>
+    </div>
+</template>
+<script setup>
+// const props = defineProps({
+
+// });
+
+</script>
